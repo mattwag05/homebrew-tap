dbe9d7683ab09fd3bf12bac755f2bda3cd2c510d
mattwag05 <wagnermatt@icloud.com>
2026-03-06 10:06:50 -0500

    pippin 0.2.1

--- Formula/pippin.rb
+++ Formula/pippin.rb
@@ -2,8 +2,8 @@ class Pippin < Formula
   desc "macOS CLI toolkit for Apple app automation"
   homepage "https://github.com/mattwag05/pippin"
   url "https://github.com/mattwag05/pippin.git",
-      tag:      "v0.2.0",
-      revision: "9e6a21755dbf6890ca9897a8fb7e036fc4f4d73d"
+      tag:      "v0.2.1",
+      revision: "4365c29b2c1a8be2d9134817dfa6000d799c126e"
   license "Apache-2.0"
   head "https://github.com/mattwag05/pippin.git", branch: "main"
 
@@ -19,6 +19,6 @@ class Pippin < Formula
   end
 
   test do
-    assert_match "0.2.0", shell_output("#{bin}/pippin --version")
+    assert_match "0.2.1", shell_output("#{bin}/pippin --version")
   end
 end
