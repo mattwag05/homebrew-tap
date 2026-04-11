e0a20df21ccd986598e37c6c0da0c83a3722f29d
mattwag05 <wagnermatt@icloud.com>
2026-04-11 17:54:02 -0400

    Add version attribute to rag-quest formula

--- Formula/rag-quest.rb
+++ Formula/rag-quest.rb
@@ -4,6 +4,7 @@ class RagQuest < Formula
   url "https://github.com/mattwag05/rag-quest/archive/9cecb53a0c23286c69049df878ef3ce8ba7505ff.tar.gz"
   sha256 :no_check  # Check this after update
   license "MIT"
+  version "0.1.0"
 
   depends_on "python@3.11"
 
